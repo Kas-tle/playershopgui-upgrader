5172c2f262082740ba5469d81930ed9e60f36392
Joshua Castle <26531652+Kas-tle@users.noreply.github.com>
2025-05-16 11:09:02 -0700

    Probably working state
    Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,23 @@
 plugins {
     application
     alias(libs.plugins.shadow)
+    alias(libs.plugins.fabric.loom)
 }
 
 repositories {
     mavenCentral()
     maven { url = uri("https://jitpack.io") }
+    maven { url = uri("https://maven.fabricmc.net/") }
 }
 
 dependencies {
+    minecraft(libs.minecraft)
+    mappings(loom.officialMojangMappings())
     implementation(libs.guava)
-    implementation(libs.data.converter)
     implementation(libs.sqlite.jdbc)
     implementation(libs.gson)
+    modImplementation(libs.fabric.loader)
+    modImplementation(libs.data.converter)
 }
 
 java {
@@ -25,6 +30,7 @@ tasks {
     val shadowJar by existing(com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar::class) {
         archiveClassifier.set("")
         archiveFileName.set("PlayerShopGUIUpgrader.jar")
+        isZip64 = true
     }
 
     build {
--- src/main/java/dev/kastle/PlayerShopGUIUpgrader.java
+++ src/main/java/dev/kastle/PlayerShopGUIUpgrader.java
@@ -1,15 +1,29 @@
 package dev.kastle;
 
+import ca.spottedleaf.dataconverter.minecraft.MCDataConverter;
+import ca.spottedleaf.dataconverter.minecraft.MCVersions;
+import ca.spottedleaf.dataconverter.minecraft.datatypes.MCTypeRegistry;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import net.minecraft.SharedConstants;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtAccounter;
+import net.minecraft.nbt.NbtFormatException;
+import net.minecraft.nbt.NbtIo;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Base64;
 
 public class PlayerShopGUIUpgrader {
     public static void main(String[] args) {
@@ -23,6 +37,7 @@ public class PlayerShopGUIUpgrader {
             Connection connection = DriverManager.getConnection("jdbc:sqlite:" + dbFile.getAbsolutePath());
             Statement statement = connection.createStatement();
         ) {
+            SharedConstants.tryDetectVersion();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM shops WHERE shopItems != '[]'");
             while (resultSet.next()) {
                 JsonElement items = JsonParser.parseString(resultSet.getString("shopItems"));
@@ -30,16 +45,76 @@ public class PlayerShopGUIUpgrader {
                     continue;
                 }
 
+
+                boolean updated = false;
                 for (JsonElement item : items.getAsJsonArray()) {
                     if (!item.isJsonObject()) {
                         continue;
                     }
 
                     JsonObject itemObject = item.getAsJsonObject();
-                    // get itemStack base64 string and do the stuff...
+                    if (!itemObject.has("itemStack")) {
+                        continue;
+                    }
+                    if (!itemObject.get("itemStack").isJsonPrimitive()) {
+                        continue;
+                    }
+                    if (!itemObject.get("itemStack").getAsJsonPrimitive().isString()) {
+                        continue;
+                    }
+
+                    byte[] decodedBytes = Base64.getDecoder().decode(itemObject.get("itemStack").getAsString());
+                    DataInputStream dataInputStream = new DataInputStream(new ByteArrayInputStream(decodedBytes));
+                    CompoundTag itemStack;
+
+                    try {
+                        itemStack = NbtIo.readCompressed(dataInputStream, NbtAccounter.unlimitedHeap());
+                    } catch (IOException | NbtFormatException e) {
+                        System.err.println("Failed to read item stack NBT data.");
+                        StringBuilder hexDump = new StringBuilder();
+                        for (byte b : decodedBytes) {
+                            hexDump.append(String.format("%02X ", b));
+                        }
+                        System.out.println("Base64 string of failed item stack: " + itemObject.get("itemStack").getAsString());
+                        System.out.println("Hex dump of failed item stack: " + hexDump.toString());
+                        e.printStackTrace(System.err);
+                        continue;
+                    }
+
+                    CompoundTag converted = MCDataConverter.convertTag(MCTypeRegistry.ITEM_STACK, itemStack, MCVersions.V1_20_1, MCVersions.V1_21_4);
+                    if (converted == null) {
+                        continue;
+                    }
+
+                    // System.out.println("Converted item stack: " + converted.getAsString());
+
+                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                    try {
+                        NbtIo.writeCompressed(converted, outputStream);
+                    } catch (IOException e) {
+                        System.err.println("Failed to write converted item stack NBT data.");
+                        e.printStackTrace(System.err);
+                        continue;
+                    }
+
+                    String base64String = Base64.getEncoder().encodeToString(outputStream.toByteArray());
+                    itemObject.addProperty("itemStack", base64String);
+                    updated = true;
                 }
-                
-                // statement.executeUpdate(...);
+
+                if (!updated) {
+                    continue;
+                }
+
+                String updateQuery = "UPDATE shops SET shopItems = ? WHERE id = ?";
+                try (PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)) {
+                    preparedStatement.setString(1, items.toString());
+                    preparedStatement.setInt(2, resultSet.getInt("id"));
+                    preparedStatement.executeUpdate();
+                } catch (SQLException e) {
+                    System.err.println("Failed to update shopItems in the database.");
+                    e.printStackTrace(System.err);
+                }
             }
         } catch (SQLException e) {
             System.err.println("Failed to connect to the database.");
